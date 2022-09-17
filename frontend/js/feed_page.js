36307a87f1c3d69d8223e34e97111be30d1d78ee
Sourayaak <sourayaabdelkader.gmail.com>
2022-09-17 16:11:57 +0300

    js: clone tweets

--- frontend/js/feed_page.js
+++ frontend/js/feed_page.js
@@ -14,7 +14,30 @@ tweet_button.addEventListener("click", function(){
     pop_up.classList.toggle("show");
 });
 
+// clone
 
+function insertAfter(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
 
+const cloneType1Tweet = () => {
+    const div = document.getElementById("tweet-type-1")
+    const body = document.getElementById("container-feed")
+    const reference = document.getElementById("tweet1-section")
+    const clone = div.cloneNode(true);
+    clone.classList.add("no-media-tweet")
+    insertAfter(clone, reference)
+}
 
+cloneType1Tweet()
 
+const cloneType2Tweet = () => {
+    const div = document.getElementById("tweet-tupe-2")
+    const body = document.getElementById("container-feed")
+    const reference = document.getElementById("tweet1-section")
+    const clone = div.cloneNode(true);
+    clone.classList.add("media-tweet")
+    insertAfter(clone, reference)
+}
+
+cloneType2Tweet()
