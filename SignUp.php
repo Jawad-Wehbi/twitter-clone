3af8c2734c9fc0c4899f06f25c440dc77a074e49
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-09-18 23:39:51 +0300

    sign in api

--- SignUp.php
+++ SignUp.php
@@ -5,25 +5,36 @@ include("connection.php");
 $name=$_POST["name"];
 $phone=$_POST["phone"];
 $email=$_POST["email"];
-$birthday = $_POST["birthday"];
+$birthday = DateTime::createFromFormat('m/d/Y', $_POST["birthday"])->format('Y-m-d');
 $password=$_POST["password"];
 
 $response = [];
 
-$emailsql="SELECT email FROM users WHERE email=$email";
+$emailsql="SELECT email FROM users WHERE email='$email'";
 $resultemail = $mysqli->query($emailsql);
-$phonesql="SELECT phone FROM users WHERE phone=$phone";
+$phonesql="SELECT phone FROM users WHERE phone='$phone'";
 $resultphone = $mysqli->query($phonesql);
 
-if ($resultemail=$email || $resultphone=$phone){
+print_r($resultemail);
+print_r($resultphone) ;
+
+if (mysqli_num_rows($resultemail) || mysqli_num_rows($resultphone)){
     $response["success"] = false;
-    $response["why"] = "email or phone";
+    echo json_encode($response);
     die();
 }
 
-$sql = "INSERT INTO users (nam, phone, email, birthday, password) value (?, ?, ?, ?, ?)";
-$mysqli->prepare($emailsql);
+$password=hash("sha256", $password);
+$password.="Jay11410";
 
-$query
+echo $password;
+
+$sql = "INSERT INTO users (name, phone, email, birthday, password) values ($name, $phone, $email, $birthday, $password)";
+$query = $mysqli->prepare($emailsql);
+$query->execute();
+
+$response["success"] = true;
+echo json_encode($response);
+
 
 ?>
