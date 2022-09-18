73ffe56da048a33d7aebe14955dc0c638a50dd30
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-09-18 15:33:11 +0300

    Log-in popup.
    Adding the log in to the landing page.

--- js/landing.js
+++ js/landing.js
@@ -1,2 +1,23 @@
 const signUpButton = document.getElementById("signUpbtn");
-const poUpsignUp = document.getElementById("pop");
+const popUpsignUp = document.getElementById("pop");
+
+const popUp = () => {
+    popUpsignUp.style.display = 'flex';
+    popUpsignUp.style.position = 'absolute';
+
+}
+
+signUpButton.addEventListener("click", popUp);
+
+
+
+const signInButton = document.getElementById("signin");
+const popUpsignIn = document.getElementById("popin");
+
+const popin = () => {
+    popUpsignIn.style.display = 'flex';
+    popUpsignIn.style.position = 'absolute';
+
+}
+
+signInButton.addEventListener("click", popin);
