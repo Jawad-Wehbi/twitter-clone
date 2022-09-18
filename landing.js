d4d277828705af51fb1a8a4824644606f45b507c
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-09-18 10:58:22 +0300

    css html edits

--- landing.js
+++
@@ -1,5 +0,0 @@
-const pop_up = document.getElementById("tweet-pop")
-const tweet_button = document.getElementById("side-tweet-button")
-tweet_button.addEventListener("click", function(){
-    pop_up.classList.toggle("show");
-});
